558d3b34057bc8ce7562ba88a25e05a3285c1e55
roshankdk <roshankdk.27@gmail.com>
2025-01-27 17:57:11 +0545

    updated log

--- utility/scrapy_runner.py
+++ utility/scrapy_runner.py
@@ -21,7 +21,7 @@ def worker(url_queue):
         })
         
         # Disable or reduce log output
-        settings.set('LOG_LEVEL', 'ERROR')
+        settings.set('LOG_LEVEL', 'DEBUG')
 
         process = CrawlerProcess(settings)
         process.crawl(SweetcareSpider.SweetcareSpider, url=url)
