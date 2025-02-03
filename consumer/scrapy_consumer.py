66bd2e5c87e130d8aaac606ce5574d770f59e5ef
roshankdk <roshankdk.27@gmail.com>
2025-02-03 17:32:48 +0545

    feat: scrapy_consumer

--- consumer/scrapy_consumer.py
+++ consumer/scrapy_consumer.py
@@ -14,7 +14,7 @@ from utility.notification import send_slack_alert
 consumer = Consumer({
     'bootstrap.servers': KAFKA_BROKER,
     'group.id': 'crawler_group',
-    'auto.offset.reset': 'latest'
+    'auto.offset.reset': 'earliest'
 })
 
 def crawl_url(url):
